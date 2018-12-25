e04a05ee009c6000a7183f69c399942d0e353c9e
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-25 01:26:22 +0100

    Kreiran konstruktor klase GeografijaDAO. U njemu se vrsi konekcija sa bazom podataka te ce biti potrebno naknadno dodati upite. U slucaju da baza sa kojom se zelimo povezati ne postoji, mapa <Grad, Drzava> se puni podacima koji su navedeni u postaci zadatka

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -6,7 +6,33 @@ import java.util.HashMap;
 
 public class GeografijaDAO {
 
-private HashMap<Drzava, Grad> mapa;
+private ArrayList<Drzava> listadrzava;
+private ArrayList<Grad> listagradova;
+
+    private GeografijaDAO () {
+
+
+        String url="jdbc:sqlite:resources/baza.db";
+        String upit = "SELECT ";
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(upit);
+        } catch (SQLException e) {
+
+            listadrzava.add(new Drzava("Francuska", "Pariz"));
+            listadrzava.add(new Drzava("Engleska","London"));
+            listadrzava.add(new Drzava("Austrija", "Beč"));
+
+            listagradova.add(new Grad("Pariz",2206488, new Drzava("Francuska", "Pariz")));
+            listagradova.add(new Grad("London",8825000, new Drzava("Engleska", "London") ));
+            listagradova.add(new Grad("Beč", 1899055,new Drzava ("Austrija", "Beč")));
+            listagradova.add(new Grad("Manchester", 545500, new Drzava ("Engleska", "London")));
+            listagradova.add(new Grad("Graz",280200, new Drzava("Austrija", "Beč")));
+
+        }
+    }
+
 
 
 }
