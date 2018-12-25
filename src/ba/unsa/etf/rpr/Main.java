7ab175cdaefced15a84a795827135b6b3d6b8d8a
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-25 12:43:07 +0100

    Implementirana funkcija ispisiGradove(). Vraca string koji sadrzi listu gradova sa njihovim informacijama

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String ispisiGradove() {
+
+        ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
+
+        String s="";
+        for(Grad g: gradovi ){
+
+            s=s+ g.getNaziv() +"("+g.getDrzava().getNaziv()+") - "+g.getBrojStanovnika()+'\n';
+        }
+        return s;
+    }
+
+
+
+
     public static void main(String[] args) {
 
 
