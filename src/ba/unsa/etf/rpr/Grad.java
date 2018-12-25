0b1114cd049bc3ef2d7805d42eca8371d0cb7652
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-25 01:28:25 +0100

    Kreirani getteri i setteri. Klasi Grad je dodan novi atribut tipa Drzava

--- src/ba/unsa/etf/rpr/Grad.java
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -3,14 +3,30 @@ package ba.unsa.etf.rpr;
 public class Grad {
 
     private String naziv;
-    private int id;
     private int broj_stanovnika;
     private Drzava drzava;
 
-    public Grad(String naziv, int id, int broj_stanovnika) {
+    public Grad(String naziv, int broj_stanovnika, Drzava drzava) {
         this.naziv = naziv;
-        this.id = id;
+        this.broj_stanovnika = broj_stanovnika;
+        this.drzava = drzava;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBroj_stanovnika() {
+        return broj_stanovnika;
+    }
+
+    public void setBroj_stanovnika(int broj_stanovnika) {
         this.broj_stanovnika = broj_stanovnika;
     }
+
 
 }
