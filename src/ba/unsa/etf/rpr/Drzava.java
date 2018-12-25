6f7debd189668d8a5a7826dd0d1b5e6a7eff69b6
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-25 01:16:30 +0100

    Kreirani getteri i setteri u klasama Drzava i Grad

--- src/ba/unsa/etf/rpr/Drzava.java
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -9,4 +9,20 @@ public class Drzava {
         this.naziv = naziv;
         this.id = id;
     }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getNazivgrada() {
+        return nazivgrada;
+    }
+
+    public void setNazivgrada(String nazivgrada) {
+        this.nazivgrada = nazivgrada;
+    }
 }
